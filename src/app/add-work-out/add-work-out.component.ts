eb7d0056fcbf7e60dc396602a7d6fc415456828a
Cyborg <sudip88.net@gmail.com>
2018-02-18 23:02:13 +0530

    test

---
+++ src/app/add-work-out/add-work-out.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-work-out',
+  templateUrl: './add-work-out.component.html',
+  styleUrls: ['./add-work-out.component.css']
+})
+export class AddWorkOutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { AddWorkOutComponent } from './add-work-out/add-work-out.component';
 
-
+const appRoutes: Routes = [
+  { path: 'Create', component: AddWorkOutComponent },
+  // { path: 'Movie', component: MovieComponent },
+  // { path: 'Currency', component: CurrencyComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AddWorkOutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
